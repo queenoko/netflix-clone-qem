77b01ee5e5dc92f2a0b99f968673244abf110353
queenoko <quimaudongwo@gmail.com>
2020-11-26 15:00:03 +0100

    first commit

---
+++ footbar.js
@@ -0,0 +1,16 @@
+
+
+// API KEY
+// acf044a6a17c92e51b189221db411a7d
+
+//API DEMO
+//  https://api.themoviedb.org/3/movie/550?api_key=acf044a6a17c92e51b189221db411a7d
+
+// //fetchTrending: `trending/all/day?api_key=${API_KEY}&language=en-US`,
+// fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+// fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+// fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+// fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+// fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+// fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+// fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
